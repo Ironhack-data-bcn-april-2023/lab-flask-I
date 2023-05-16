40024bb49cd79286b68d3efb77bcf2adbd09fb92
Bego Ripoll <bripollc@gmail.com>
2023-05-16 17:26:21 +0200

    Flask I+II+III

--- small-api/main.py
+++ small-api/main.py
@@ -24,6 +24,25 @@ def example ():
 def example_table (tablename):
     return queries.table_ten(tablename)
 
+
+#FLASK III
+
+@app.route("/insert-into-employees", methods =["POST"])
+def example_insert_db ():
+    dict_ = {
+    "emp_no": request.args["emp_no"],
+    "birth_date": request.args["birth_date"],
+    "first_name": request.args["first_name"],
+    "last_name": request.args["last_name"],
+    "gender": request.args["gender"],
+    "hire_date": request.args["hire_date"]
+    } 
+
+    return queries.insert_new_employee(dict_)
+
+
+#FLASK III
+
 if __name__ == "__main__":
     app.run(port=9000, debug=False)
 
--- small-api/tools/sql_queries.py
+++ small-api/tools/sql_queries.py
@@ -13,3 +13,31 @@ def table_ten(tablename):
     engine = conn.connection_to_sql()
     df = pd.read_sql_query(query, engine)
     return df.to_dict(orient="records")
+
+
+
+def table_ten(tablename):
+    query = f"SELECT * FROM {tablename} LIMIT 10;"
+    engine = conn.connection_to_sql()
+    df = pd.read_sql_query(query, engine)
+    return df.to_dict(orient="records")
+
+
+#FLASK III
+
+
+def insert_new_employee (dict_):
+    emp_no = dict_["emp_no"]
+    birth_date = dict_["birth_date"]
+    first_name = dict_["first_name"]
+    last_name = dict_["last_name"]
+    gender = dict_["gender"]
+    hire_date = dict_["hire_date"]
+
+    query = f"INSERT INTO employees VALUES ('{emp_no}', '{birth_date}', '{first_name}', '{last_name}', '{gender}', '{hire_date}');"
+    engine = conn.connection_to_sql()
+    engine.execute(query)
+    return "New employee inserted"
+
+
+#FLASK III
